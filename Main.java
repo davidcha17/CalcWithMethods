fd10b7d87eceb3c85bd37b186c6da6cf4a10d90a
David Cha <littlesadtea@gmail.com>
2020-09-25 23:47:11 -0400

    first commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("hi");
+    }
+}
